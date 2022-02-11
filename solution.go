53ffa82c0dc2be44b02ba1eaa8f2bddda79885d0
Slobodskoy <slobodskoy.a.v@yandex.ru>
2022-02-11 22:13:08 +0500

    Continue

--- solution.go
+++ solution.go
@@ -1,13 +1,9 @@
 package solution
 
 import (
-	"fmt"
-	"strings"
-
 	"github.com/kyokomi/emoji/v2"
 )
 
 func GetMessage() string {
-	return fmt.Sprintf("%s!",
-		strings.TrimRight(emoji.Sprint("Hello :world_map:"), " "))
+	return emoji.Sprint("Hello :world_map:!")
 }
--- solution_test.go
+++ solution_test.go
@@ -1,12 +1,22 @@
 package solution
 
 import (
+	"strings"
 	"testing"
 )
 
 func TestSolution(t *testing.T) {
 	message := GetMessage()
-	if message != "Hello 🗺️!" {
+	if message != "Hello 🗺️ !" {
 		t.Error()
+	}
+}
+
+func TestGetMessage(t *testing.T) {
+	expected := string([]rune{72, 101, 108, 108, 111, 32, 128506, 65039, 32, 33})
+	msg := GetMessage()
+
+	if !strings.EqualFold(msg, expected) {
+		t.Errorf("Unexpected result:\n\tExpected: %q\n\tGot: %q", expected, msg)
 	}
 }
